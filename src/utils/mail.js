86a51f96b52527882563f039f365a0506502f51a
Anubhav Mandal <anubhav.mandal0306@gmail.com>
2026-01-18 13:14:40 +0530

    include mailgen

---
+++ src/utils/mail.js
@@ -0,0 +1,43 @@
+import Mailgen from "mailgen";
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! We are excited to have you on board.",
+      action: {
+        instructions:
+          "To verify your email please click on the following button.",
+        button: {
+          color: "#F54927",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of your account.",
+      action: {
+        instructions:
+          "To reset your password click on the following button or link.",
+        button: {
+          color: "#F52787",
+          text: "Reset Password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
