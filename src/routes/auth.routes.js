d3aa01cd94989f17a4b4bcbc6dda376c250f420d
Anubhav Mandal <anubhav.mandal0306@gmail.com>
2026-04-18 15:16:00 +0530

    add login validators

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { registerUser, login } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator } from "../validators/index.js";
+import {
+  userRegisterValidator,
+  userLoginValidator,
+} from "../validators/index.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
-router.route("/login").post(login); //no validation added yet
+router.route("/login").post(userLoginValidator(), validate, login);
 
 export default router;
